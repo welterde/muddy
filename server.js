8e5020008af63c78961206736ee0e037110eac4f
Danny Tatom <dannytatom@gmail.com>
2010-08-05 23:18:46 -0700

    Added config file for MUD host & port

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var net = require('net'),
-    ws  = require('websocket-server'),
-    server, mud
+var net  = require('net'),
+    ws   = require('websocket-server'),
+    fs   = require('fs'),
+    yaml = require('yaml'),
+    server, mud, config
+
+config = yaml.eval(fs.readFileSync('config.yml', 'utf8'))
 
 server = ws.createServer()
 server.addListener('connection', function(connection) {
-  mud = net.createConnection(3000, 'primordiax.com')
+  mud = net.createConnection(config.port, config.host)
   mud.setEncoding('utf8')
 
   mud.addListener('data', function(data) {
