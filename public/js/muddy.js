940a2d67342ba95230c8d27836975ba8b9479ed6
Danny Tatom <dannytatom@gmail.com>
2010-11-26 19:08:02 -0800

    Moved color code formatting the client side JS

--- public/js/muddy.js
+++ public/js/muddy.js
@@ -3,13 +3,43 @@ var window_focused = true
   , curHistory
   , socket
 
+var format = function(data) {
+  data = data + '\n'
+
+  return data.replace(/;40/g,     '')
+             .replace(/�/g,       '\n')
+             .replace(/\[0m/g,    "</span>")
+             .replace(/\[0;30m/g, "<span class='black'>")
+             .replace(/\[1;30m/g, "<span class='black bold'>")
+             .replace(/\[0;31m/g, "<span class='red'>")
+             .replace(/\[1;31m/g, "<span class='red bold'>")
+             .replace(/\[0;32m/g, "<span class='green'>")
+             .replace(/\[1;32m/g, "<span class='green bold'>")
+             .replace(/\[0;33m/g, "<span class='yellow'>")
+             .replace(/\[1;33m/g, "<span class='yellow bold'>")
+             .replace(/\[0;34m/g, "<span class='blue'>")
+             .replace(/\[1;34m/g, "<span class='blue bold'>")
+             .replace(/\[0;35m/g, "<span class='magenta'>")
+             .replace(/\[1;35m/g, "<span class='magenta bold'>")
+             .replace(/\[0;36m/g, "<span class='cyan'>")
+             .replace(/\[1;36m/g, "<span class='cyan bold'>")
+             .replace(/\[0;37m/g, "<span class='white'>")
+             .replace(/\[1;37m/g, "<span class='white bold'>")
+             .replace(/\[1m/g,    "<span class='bold'>")
+             .replace(/\[3m/g,    "<span class='italic'>")
+             .replace(/\[4m/g,    "<span class='underline'>")
+             .replace(/\[22m/g,   "<span class='no-bold'>")
+             .replace(/\[23m/g,   "<span class='no-italic'>")
+             .replace(/\[24m/g,   "<span class='no-underline'>")
+}
+
 var setTerminalSize = function() {
   var wWidth  = window.innerWidth
-    , wHeight = window.innerHeight;
+    , wHeight = window.innerHeight
 
-  $('#container').css('width',  (wWidth - 20));
-  $('#container').css('height', (wHeight - 20));
-  $('#output').css('height', (wHeight - 50));
+  $('#container').css('width',  (wWidth - 20))
+  $('#container').css('height', (wHeight - 20))
+  $('#output').css('height', (wHeight - 50))
 }
 
 var sendCommand = function(command) {
@@ -18,68 +48,68 @@ var sendCommand = function(command) {
   $('#output pre').append("<span class='self'>> " + command + "</span>\n")
 
   if (command != '') {
-    cmdHistory.push(command);
+    cmdHistory.push(command)
     curHistory = (cmdHistory.length - 1)
   }
 
-  lockScroll();
+  lockScroll()
 }
 
 var updateTerminal = function(data) {
-  $('#output pre').append(data);
+  $('#output pre').append(format(data))
 
   if (!window_focused) {
-    document.title = 'muddy *';
+    document.title = 'muddy *'
   }
 
-  lockScroll();
+  lockScroll()
 }
 
 var lockScroll = function() {
-  $('#output').attr({ scrollTop: $('#output').attr('scrollHeight') });
+  $('#output').attr({ scrollTop: $('#output').attr('scrollHeight') })
 }
 
 var initialize = function() {
-  setTerminalSize();
-  window.onblur  = function() { window_focused = false; }
-  window.onfocus = function() { window_focused = true;  }
+  setTerminalSize()
+  window.onblur  = function() { window_focused = false }
+  window.onfocus = function() { window_focused = true  }
  
   $(window).focus(function() {
-    document.title = 'muddy';
-  });
+    document.title = 'muddy'
+  })
 
   $(window).resize(function() {
-    setTerminalSize();
-  });
+    setTerminalSize()
+  })
 
-  $('#input input').focus();
+  $('#input input').focus()
   $('#input input').keyup(function(event) {
     if (event.keyCode == 13) {
       var command = $('#input input').val()
 
-      sendCommand(command);
-      $('#input input').val('');
+      sendCommand(command)
+      $('#input input').val('')
     } else if (event.keyCode == 38) {
       if (curHistory >= 0) {
-        $('#input input').val(cmdHistory[curHistory]);
-        curHistory -= 1;
+        $('#input input').val(cmdHistory[curHistory])
+        curHistory -= 1
       }
     } else if (event.keyCode == 40) {
       if (curHistory <= (cmdHistory.length - 1)) {
-        curHistory += 1;
-        $('#input input').val(cmdHistory[curHistory]);
+        curHistory += 1
+        $('#input input').val(cmdHistory[curHistory])
       }
     }
-  });
+  })
 
-  socket = new io.Socket('localhost');
-  socket.connect();
+  socket = new io.Socket('localhost')
+  socket.connect()
 
   socket.on('message', function(data) {
-    updateTerminal(data);
-  });
+    updateTerminal(data)
+  })
 }
 
 $(function() {
   initialize()
-});
+})
